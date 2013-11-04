ad5e897fe8709eb39c032c75f1202ea51e3f586a
omgjens <jensvanlangen@gmail.com>
2013-11-04 22:04:57 +0100

    tekst roteer werkt

--- ObjectControl.cs
+++ ObjectControl.cs
@@ -125,6 +125,55 @@ namespace SchetsEditor
             return false;
         }
 
+        // Deze methode bepaalt of het punt p binnen de geaccepteerde marge t.o.v. het lijnstuk ligt
+        private static bool isOpLijnGeklikt(TekenObject obj, Point p)
+        {
+            return (afstandVanPuntTotLijn(obj.Points[0], obj.Points[1], p) < GumRandDikte);
+        }
+
+        // Bepaalt of het punt binnen het object ligt en geeft de mogelijkheid om de grootte van de rechthoek aan te passen
+        private static bool isBinnenVierkant(TekenObject obj, Point p, int aanpassing)
+        {
+            return (p.X > obj.Points[0].X + aanpassing && p.X < obj.Points[1].X - aanpassing && 
+                    p.Y > obj.Points[0].Y + aanpassing && p.Y < obj.Points[1].Y - aanpassing);
+        }
+
+        // Laat weten of er op de rand van de rechthoek geklikt is, het kader wordt aan weerskanten vergroot met de helft van de dikte van de gumrand
+        private static bool isOpKaderGeklikt(TekenObject obj, Point p)
+        {
+            return (!isBinnenVierkant(obj, p, GumRandDikte / 2) && isBinnenVierkant(obj, p, -(GumRandDikte / 2)));
+        }
+
+        // Bepaalt of punt p binnen of op de lijn van de cirkel ligt afhankelijk van de waarde van de boolean isGevuldeCirkel
+        private static bool isEclipseRaakGeklikt(TekenObject obj, Point p, bool isGevuldeCirkel)
+        {
+            Point begin = new Point(Math.Min(obj.Points[0].X, obj.Points[1].X), Math.Min(obj.Points[0].Y, obj.Points[1].Y));
+            Point eind = new Point(Math.Max(obj.Points[0].X, obj.Points[1].X), Math.Max(obj.Points[0].Y, obj.Points[1].Y));
+            double radiusx = Math.Abs((double)eind.X - (double)begin.X) / 2.0;
+            double radiusy = Math.Abs((double)eind.Y - (double)begin.Y) / 2.0;
+            double middelpuntx = begin.X + radiusx;
+            double middelpunty = begin.Y + radiusy;
+
+            if (isGevuldeCirkel)
+                return isPuntBinnenCirkel(p, radiusx, radiusy, middelpuntx, middelpunty);
+            else
+                return isPuntOpRand(p, radiusx, radiusy, middelpuntx, middelpunty);
+        }
+
+        // Laat weten of er op de rand van de ellips geklikt is, de ellips wordt aan weerskanten vergroot met de helft van de dikte van de gumrand
+        // Maakt gebruik van de stelling van pythagoras om de afstand te berekenen t.o.v. het middelpunt
+        private static bool isPuntOpRand(Point p, double radiusx, double radiusy, double middelpuntx, double middelpunty)
+        {
+            return (Math.Sqrt(Math.Pow((p.X - middelpuntx) / (radiusx - GumRandDikte), 2) + Math.Pow((p.Y - middelpunty) / (radiusy - GumRandDikte), 2)) >= 1 
+                && isPuntBinnenCirkel(p, radiusx, radiusy, middelpuntx, middelpunty));
+        }
+
+        // 
+        private static bool isPuntBinnenCirkel(Point p, double radiusx, double radiusy, double middelpuntx, double middelpunty)
+        {
+            return (Math.Sqrt(Math.Pow((p.X - middelpuntx) / radiusx, 2) + Math.Pow((p.Y - middelpunty) / radiusy, 2)) <= 1);
+        }
+
         // Deze methode is afgeleid van http://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
         // en berekent de kortste afstand vanaf punt p tot aan de lijn die loop vanaf het punt begin tot het punt eind
         private static double afstandVanPuntTotLijn(Point begin, Point eind, Point p)
@@ -158,57 +207,6 @@ namespace SchetsEditor
 
             return Math.Sqrt(Math.Pow(p.X - xx, 2) + Math.Pow(p.Y - yy, 2));
         }
-
-        // Deze methode bepaalt of het punt p binnen de geaccepteerde marge t.o.v. het lijnstuk ligt
-        private static bool isOpLijnGeklikt(TekenObject obj, Point p)
-        {
-            return (afstandVanPuntTotLijn(obj.Points[0], obj.Points[1], p) < GumRandDikte);
-        }
-
-        // Bepaalt of het punt binnen het object ligt en geeft de mogelijkheid om de grootte van de rechthoek aan te passen
-        private static bool isBinnenVierkant(TekenObject obj, Point p, int aanpassing)
-        {
-            return (p.X > obj.Points[0].X + aanpassing && p.X < obj.Points[1].X - aanpassing && 
-                    p.Y > obj.Points[0].Y + aanpassing && p.Y < obj.Points[1].Y - aanpassing);
-        }
-
-        // Laat weten of er op de rand van de rechthoek geklikt is, het kader wordt aan weerskanten vergroot met de helft van de dikte van de gumrand
-        private static bool isOpKaderGeklikt(TekenObject obj, Point p)
-        {
-            return (!isBinnenVierkant(obj, p, GumRandDikte / 2) && isBinnenVierkant(obj, p, -(GumRandDikte / 2)));
-        }
-
-        // 
-        private static bool isPuntOpRand(Point p, double radiusx, double radiusy, double middelpuntx, double middelpunty)
-        {
-            return (Math.Sqrt(Math.Pow((p.X - middelpuntx) / (radiusx - GumRandDikte), 2) + Math.Pow((p.Y - middelpunty) / (radiusy - GumRandDikte), 2)) >= 1 
-                && isPuntBinnenCirkel(p, radiusx, radiusy, middelpuntx, middelpunty));
-        }
-
-        private static bool isPuntBinnenCirkel(Point p, double radiusx, double radiusy, double middelpuntx, double middelpunty)
-        {
-            return (Math.Sqrt(Math.Pow((p.X - middelpuntx) / radiusx, 2) + Math.Pow((p.Y - middelpunty) / radiusy, 2)) <= 1);
-        }
-
-        // Bepaalt of punt p binnen of op de lijn van de cirkel ligt afhankelijk van de waarde van de boolean isGevuldeCirkel
-        private static bool isEclipseRaakGeklikt(TekenObject obj, Point p, bool isGevuldeCirkel)
-        {
-            Point begin = new Point(Math.Min(obj.Points[0].X, obj.Points[1].X), Math.Min(obj.Points[0].Y, obj.Points[1].Y));
-            Point eind = new Point(Math.Max(obj.Points[0].X, obj.Points[1].X), Math.Max(obj.Points[0].Y, obj.Points[1].Y));
-            double radiusx = Math.Abs((double)eind.X - (double)begin.X) / 2.0;
-            double radiusy = Math.Abs((double)eind.Y - (double)begin.Y) / 2.0;
-            double middelpuntx = begin.X + radiusx;
-            double middelpunty = begin.Y + radiusy;
-
-            if (isGevuldeCirkel)
-            {
-                return isPuntBinnenCirkel(p, radiusx, radiusy, middelpuntx, middelpunty);
-            }
-            else
-            {
-                return isPuntOpRand(p, radiusx, radiusy, middelpuntx, middelpunty);
-            }
-        }
     }
 
     public class DrawFromXML
@@ -226,16 +224,29 @@ namespace SchetsEditor
                 switch (obj.Tool)
                 {
                     case "tekst":
-                        
-                        SizeF sz = gr.VisibleClipBounds.Size;
-                        gr.TranslateTransform(sz.Width / 2, sz.Height / 2);
-                        gr.RotateTransform(obj.Hoek);
-                        sz = gr.MeasureString(obj.Tekst, font);
-                        gr.DrawString(obj.Tekst, font, Brushes.Black, -(sz.Width / 2) + obj.Points[0].X, -(sz.Height / 2) + obj.Points[0].Y);
-                        gr.ResetTransform();
-
-                        //gr.DrawString(obj.Tekst, font, brush, obj.Points[0], StringFormat.GenericDefault);
-                        //gr.ResetTransform();
+                        SizeF sz = gr.MeasureString(obj.Tekst, font);
+                        Bitmap tekstBmp = new Bitmap((int)sz.Width, (int)sz.Height);
+                        Graphics gOff = Graphics.FromImage(tekstBmp);
+                        gOff.DrawString(obj.Tekst, font, Brushes.Black, new Point(0, 0));
+                        if (obj.Hoek == 90)
+                        {
+                            tekstBmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                            gr.DrawImage(tekstBmp, obj.Points[0].X - (int)sz.Height, obj.Points[0].Y);
+                        }
+                        else if (obj.Hoek == 180)
+                        {
+                            tekstBmp.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                            gr.DrawImage(tekstBmp, obj.Points[0].X - (int)sz.Width, obj.Points[0].Y - (int)sz.Height);
+                        }
+                        else if (obj.Hoek == 270)
+                        {
+                            tekstBmp.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                            gr.DrawImage(tekstBmp, obj.Points[0].X, obj.Points[0].Y - (int)sz.Width);
+                        }
+                        else
+                        {
+                            gr.DrawImage(tekstBmp, obj.Points[0]);
+                        }
                         break;
                     case "kader":
                         new RechthoekTool().Teken(gr, obj.Points[0], obj.Points[1], brush);
