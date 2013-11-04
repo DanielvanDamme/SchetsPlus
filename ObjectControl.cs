e1a18bd8ad7ff34507aeb4f3965a5d68f6c1467d
omgjens <jensvanlangen@gmail.com>
2013-11-04 16:24:39 +0100

    Werkend gummen rondje, cirkel, vierkant

--- ObjectControl.cs
+++ ObjectControl.cs
@@ -45,13 +45,84 @@ namespace SchetsEditor
         {
             for (int i = (objecten.Count - 1); i >= 0; i--)
             {
-                if (p.X > objecten[i].Points[0].X && p.X < objecten[i].Points[1].X && p.Y > objecten[i].Points[0].Y && p.Y < objecten[i].Points[1].Y)
+                if (isRaak(objecten[i], p))
                 {
                     objecten.RemoveAt(i);
                     break;
                 }
             }
         }
+
+        private bool isRaak(TekenObject obj, Point p)
+        {
+            switch (obj.Tool)
+            {
+                case "tekst":
+
+                    return true;
+                case "kader":
+
+                    return true;
+                case "vlak":
+                    if (p.X > obj.Points[0].X && p.X < obj.Points[1].X && p.Y > obj.Points[0].Y && p.Y < obj.Points[1].Y)
+                        return true;
+                    break;
+                case "cirkel":
+                    return eclipseRaak(obj, p, false);
+                case "rondje":
+                    return eclipseRaak(obj, p, true);
+                    break;
+                case "lijn":
+
+                    return true;
+                case "pen":
+
+                    return true;
+            }
+            return false;
+        }
+
+        private bool eclipseRaak(TekenObject obj, Point p, bool vollecirkel)
+        {
+            Point p1 = new Point(Math.Min(obj.Points[0].X, obj.Points[1].X), Math.Min(obj.Points[0].Y, obj.Points[1].Y));
+            Point p2 = new Point(Math.Max(obj.Points[0].X, obj.Points[1].X), Math.Max(obj.Points[0].Y, obj.Points[1].Y));
+            double radiusx = Math.Abs((double)p2.X - (double)p1.X) / 2.0;
+            double radiusy = Math.Abs((double)p2.Y - (double)p1.Y) / 2.0;
+            double middelpuntx = p1.X + radiusx;
+            double middelpunty = p1.Y + radiusy;
+
+            if (vollecirkel)
+            {
+                return helecirkel(p, radiusx, radiusy, middelpuntx, middelpunty);
+            }
+            else
+            {
+                return cirkelrand(p, radiusx, radiusy, middelpuntx, middelpunty);
+            }
+        }
+
+        private bool cirkelrand(Point p, double radiusx, double radiusy, double middelpuntx, double middelpunty)
+        {
+            const int PenSize = 3;
+            if (Math.Sqrt(Math.Pow((p.X - middelpuntx) / (radiusx - PenSize), 2) + Math.Pow((p.Y - middelpunty) / (radiusy - PenSize), 2)) >= 1 &&
+                helecirkel(p, radiusx, radiusy, middelpuntx, middelpunty))
+                return true;
+            else
+                return false;
+        }
+
+        private bool helecirkel(Point p, double radiusx, double radiusy, double middelpuntx, double middelpunty)
+        {
+            if (Math.Sqrt(Math.Pow((p.X - middelpuntx) / radiusx, 2) + Math.Pow((p.Y - middelpunty) / radiusy, 2)) <= 1)
+                return true;
+            else
+                return false;
+        }
+
+
+
+        
+        
 
         public void actieTerugdraaien()
         {
